4ac145cb959e669bbad9525410fef10ed929d051
Rafael Armenio <rafael.armenio@gmail.com>
2016-05-12 22:34:22 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// ini_set('display_errors', 'On');
+ini_set('display_errors', 'Off');
+// error_reporting(E_ALL ^ E_USER_DEPRECATED ^ E_STRICT ^ E_NOTICE ^ E_WARNING);
+// error_reporting(E_ALL);
+error_reporting(0);
+define('ROOT_PATH', __DIR__);
+
+include 'public/index.php';
